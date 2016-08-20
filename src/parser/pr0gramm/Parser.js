823f3fb96b7a9f46ca213a1927fc1ae1f2da74bd
Fa!ko Krause <falko@webpgr.com>
2016-08-20 15:11:26 +0200

    fixed getting first page, key for next page is in differen key in stream

--- src/parser/pr0gramm/Parser.js
+++ src/parser/pr0gramm/Parser.js
@@ -8,11 +8,15 @@ export default class extends BaseParser {
 		this.baseURL = 'https://pr0gramm.com/api/items/get?flags=1&promoted=1';
 		this.lastKey = '';
 		this.parserName = 'pr0gramm API';
+		this.isRunning = false;
 	}
 
 	getVideosFromIndex() {
 		const url = !this.lastKey ? this.baseURL : `https://pr0gramm.com/api/items/get?older=${this.lastKey}&flags=1&promoted=1`;
+		if (this.isRunning) return;
+		this.isRunning = true;
 		return this.ajax(url).then(rawJsonIndex => {
+			this.isRunning = false;
 			const stream = JSON.parse(rawJsonIndex);
 			this.videos = [
 				...this.videos,
@@ -23,7 +27,7 @@ export default class extends BaseParser {
 						mp4: `https://vid.pr0gramm.com/${item.image}`,
 					})),
 			];
-			this.lastKey = stream.cache.replace('stream:top:1:o', '');
+			this.lastKey = !this.lastKey ? stream.items[stream.items.length - 1].promoted : stream.cache.replace('stream:top:1:o', '');
 		});
 	}
 }
