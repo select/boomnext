99cf3d369e86634ca0b99e6a8f8ab09544ae3002
Fa!ko Krause <falko@webpgr.com>
2016-08-28 19:07:42 +0200

    formatting and removed quota check, going for infinite space

--- src/IndexDBVideos.js
+++ src/IndexDBVideos.js
@@ -1,19 +1,23 @@
 export default class {
-	constructor() {
+	constructor(gui) {
 		this.isReady = false;
 		this.collectionName = 'videos';
 
-		// -----------------------
-		// Request storage usage and capacity left
-		navigator.webkitTemporaryStorage.queryUsageAndQuota(
-			(used, granted) => {
-				const percent = 100 * used / granted;
-				console.log(`## ${percent.toPrecision(2)}% storage used`);
-			},
-			error => {
-				console.log('Error', error);
-			}
-		);
+		// // -----------------------
+		// // Request storage usage and capacity left
+		// navigator.webkitTemporaryStorage.queryUsageAndQuota(
+		// 	(used, granted) => {
+		// 		console.log('granted: ',granted);
+		// 		const percent = 100 * used / granted;
+		// 		if (percent === 100) gui.warn('IndexDB full!');
+		// 		console.log(`## ${percent.toPrecision(2)}% storage used`);
+		// 	},
+		// 	error => {
+		// 		console.log('Error', error);
+		// 	}
+		// );
+
+
 		// -----------------------
 
 		//No support? Go in the corner and pout.
@@ -38,6 +42,7 @@ export default class {
 		openRequest.onsuccess = (event) => {
 			this.isReady = true;
 			this.db = event.target.result;
+			// this.getAll();
 		};
 
 		openRequest.onerror = (event) => {
@@ -60,13 +65,8 @@ export default class {
 				.transaction([this.collectionName], 'readwrite')
 				.objectStore(this.collectionName)
 				.add(data);
-			request.onerror = (event) => {
-				reject(`Error ${event.target.error.name}`);
-			};
-
-			request.onsuccess = () => {
-				resolve();
-			};
+			request.onerror = event => reject(`Error ${event.target.error.name}`);
+			request.onsuccess = () => resolve();
 		});
 		return promise;
 	}
@@ -77,14 +77,29 @@ export default class {
 				.transaction([this.collectionName], 'readonly')
 				.objectStore(this.collectionName)
 				.get(id);
-			request.onerror = (event) => {
-				reject(`Error ${event.target.error.name}`);
-			};
-
-			request.onsuccess = (event) => {
-				resolve(event.target.result);
-			};
+			request.onerror = event => reject(`Error ${event.target.error.name}`);
+			request.onsuccess = event => resolve(event.target.result);
 		});
 		return promise;
 	}
+
+	// getAll() {
+	// 	const request = this.db
+	// 		.transaction(this.collectionName, 'readonly')
+	// 		.objectStore(this.collectionName)
+	// 		.openCursor();
+	// 	request.onerror = event => console.warn('error reading db');
+	// 	let sum = 0;
+	// 	request.onsuccess = event => {
+	// 		const cursor = event.target.result;
+	// 		if (cursor) {
+	// 			const json = JSON.stringify(cursor.value);
+	// 			console.log(json.length);
+	// 			sum += json.length;
+	// 			cursor.continue();
+	// 		}else {
+	// 			console.log('sum: ',sum);
+	// 		}
+	// 	};
+	// }
 }
